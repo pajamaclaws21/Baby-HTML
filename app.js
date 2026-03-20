969c143d21706ebab7d76256eee886c12facb153
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2026-03-20 11:30:07 -0500

    fixing some stuff

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ function appInit(){
     // setting up custom console
     console.log("setting up custom console");
     var myConsole = document.getElementById("console-2dgds709ga");
-    console.clear = function(x){myConsole.innerHTML += `<p>Console Cleared</p><br>`};
-    console.log = function(x){myConsole.innerHTML += `<p style="color: black">Log ${x}</p><br>`};
-    console.error = function(x){myConsole.innerHTML += `<p style="color: red">Err ${x}</p><br>`};
-    console.debug = function(x){myConsole.innerHTML += `<p style="color: blue">Debug ${x}</p><br>`};
-    console.info = function(x){myConsole.innerHTML += `<p style="color: green">Info ${x}</p><br>`};
-    console.warn = function(x){myConsole.innerHTML += `<p style="color: pink">Warn ${x}</p><br>`};
+    console.clear = function(x){myConsole.innerHTML += `<span>Console Cleared</span><br>`};
+    console.log = function(x){myConsole.innerHTML += `<span style="color: black">${x}</span><br>`};
+    console.error = function(x){myConsole.innerHTML += `<span style="color: red"><b>Err</b> ${x}</span><br>`};
+    console.debug = function(x){myConsole.innerHTML += `<span style="color: blue"><b>Debug</b> ${x}</span><br>`};
+    console.info = function(x){myConsole.innerHTML += `<span style="color: green"><b>Info</b> ${x}</span><br>`};
+    console.warn = function(x){myConsole.innerHTML += `<span style="color: pink"><b>Warn</b> ${x}</span><br>`};
 
     // listening for console messages from iframe
     console.log("Console is ready! Try me~");
@@ -28,15 +28,16 @@ function appInit(){
         if (event.data = "clear") console.clear();
         try {
             let data = JSON.parse(event.data);
-            if (data[0] == "log") console.log(data[1]) 
-            else if (data[0] == "error") console.error(data[1])
-            else if (data[0] == "debug") console.debug(data[1])
-            else if (data[0] == "info") console.info(data[1])
-            else if (data[0] == "warn") console.warn(data[1])
-            else console.log(`Console method ${data[0]} called with message: ${data[1]}`);
         } catch {
             console.log(`JSON parse failed with message: ${event.data}`);
         }
+
+        if (data[0] == "log") console.log(data[1]) 
+        else if (data[0] == "error") console.error(data[1])
+        else if (data[0] == "debug") console.debug(data[1])
+        else if (data[0] == "info") console.info(data[1])
+        else if (data[0] == "warn") console.warn(data[1])
+        else console.log(`Console "${data[0]}": "${data[1]}"`);
     });
 };
 
