d8516d66815d431757d7dc1c3b2db1977c148902
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2026-03-20 14:20:16 -0500

    lint

--- app.js
+++ app.js
@@ -15,29 +15,29 @@ function appInit(){
     // setting up custom console
     console.log("setting up custom console");
     var myConsole = document.getElementById("console-2dgds709ga");
-    console.clear = function(x){myConsole.innerHTML += `<span>Console Cleared</span><br>`};
-    console.log = function(x){myConsole.innerHTML += `<span style="color: black">${x}</span><br>`};
-    console.error = function(x){myConsole.innerHTML += `<span style="color: red"><b>Err</b> ${x}</span><br>`};
-    console.debug = function(x){myConsole.innerHTML += `<span style="color: blue"><b>Debug</b> ${x}</span><br>`};
-    console.info = function(x){myConsole.innerHTML += `<span style="color: green"><b>Info</b> ${x}</span><br>`};
-    console.warn = function(x){myConsole.innerHTML += `<span style="color: pink"><b>Warn</b> ${x}</span><br>`};
+    console.clear = () => myConsole.innerHTML += `<span>Console Cleared</span><br>`;
+    console.log = (x) => myConsole.innerHTML += `<span style="color: black">${x}</span><br>`;
+    console.error = (x) => myConsole.innerHTML += `<span style="color: red"><b>Err</b> ${x}</span><br>`;
+    console.debug = (x) => myConsole.innerHTML += `<span style="color: blue"><b>Debug</b> ${x}</span><br>`;
+    console.info = (x) => myConsole.innerHTML += `<span style="color: green"><b>Info</b> ${x}</span><br>`;
+    console.warn = (x) => myConsole.innerHTML += `<span style="color: pink"><b>Warn</b> ${x}</span><br>`;
 
     // listening for console messages from iframe
     window.addEventListener("message", (event) => {
-        if (event.data = "clear") console.clear();
-        if (event.data = "console ready") console.log("Console is ready! Try me~");
+        if (event.data = "clear"){ console.clear(); };
+        if (event.data = "console ready"){ console.log("Console is ready! Try me~"); };
         try {
             let data = JSON.parse(event.data);
         } catch {
             console.log(`JSON parse failed with message: ${event.data}`);
         }
 
-        if (data[0] == "log") console.log(data[1]) 
-        else if (data[0] == "error") console.error(data[1])
-        else if (data[0] == "debug") console.debug(data[1])
-        else if (data[0] == "info") console.info(data[1])
-        else if (data[0] == "warn") console.warn(data[1])
-        else console.log(`Console "${data[0]}": "${data[1]}"`);
+        if (data[0] == "log"){ console.log(data[1]); }
+        else if (data[0] == "error"){ console.error(data[1]); }
+        else if (data[0] == "debug"){ console.debug(data[1]); }
+        else if (data[0] == "info"){ console.info(data[1]); }
+        else if (data[0] == "warn"){ console.warn(data[1]); }
+        else { console.log(`Console "${data[0]}": "${data[1]}"`); };
     });
 };
 
--- iframe.js
+++ iframe.js
@@ -1,21 +1,18 @@
-var bHPage;
-var bHOrigin;
+var source;
 
 // See https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
 window.addEventListener("message", (event) => {
-    if (!bHPage) bHPage = event.source;
-    bHPage.postMessage("console ready");
+    source = event.source;
+    source.postMessage("console ready");
 
     // handle console functions
-    console.clear = function(){ bHPage.postMessage("clear")  };
-    console.log = function(x){  bHPage.postMessage(JSON.stringify(["log", x]))  };
-    console.error = function(x){bHPage.postMessage(JSON.stringify(["error", x]))  };
-    console.debug = function(x){bHPage.postMessage(JSON.stringify(["debug", x]))  };
-    console.info = function(x){ bHPage.postMessage(JSON.stringify(["info", x]))  };
-    console.warn = function(x){ bHPage.postMessage(JSON.stringify(["warn", x]))  };
+    console.clear = () => source.postMessage("clear");
+    console.log = (x) => source.postMessage(JSON.stringify(["log", x]));
+    console.error = (x) => source.postMessage(JSON.stringify(["error", x]));
+    console.debug = (x) => source.postMessage(JSON.stringify(["debug", x]));
+    console.info = (x) => source.postMessage(JSON.stringify(["info", x]));
+    console.warn = (x) => source.postMessage(JSON.stringify(["warn", x]));
 });
 
 // Handle JS errors
-window.onerror = (message, source, lineno, colno, error) => {
-    console.error(`error at ${source} (${lineno},${colno}): ${message}`);
-};
+window.onerror = (message, loc, lineno, colno, error) => console.error(`error at ${loc} (${lineno},${colno}): ${message}`);
