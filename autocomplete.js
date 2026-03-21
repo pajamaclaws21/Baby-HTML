e2e6d5b653bb2031e502f35918683510f11058ac
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2026-03-21 18:18:57 -0500

    fixing autocomplete

--- autocomplete.js
+++ autocomplete.js
@@ -1,3 +1,7 @@
+var tagName;
+var listeningForTag = false;
+var input = document.getElementById("text-12949124a09s70a");
+
 function isTypable(x){
     let alphabet = "qwertyuiopasdfghjklzxcvbnm";
     let numbers = "1234567890";
@@ -5,36 +9,61 @@ function isTypable(x){
     return (special.includes(x) || numbers.includes(x) || alphabet.includes(x) || x == '"');
 }
 
+function insertString(x){
+    // from https://bennadel.com/4086
+    var code = input.value;
+    var cursorPosition = input.selectionEnd;
+    // where cursor will be after paste
+    var finalPosition = (cursorPosition + x.length);
+    // textarea value is equal to the code up to cursor + tag + code after cursor
+    input.value = code.slice(0, cursorPosition) + x + code.slice(cursorPosition);
+    // move cursor
+    input.selectionStart = finalPosition;
+    input.selectionEnd = finalPosition;
+}
+
 window.addEventListener("keydown", (event) => {
     var key = event.key;
-    var listeningForTag = false;
-    var tagName = "";
-
-    var input = document.getElementById("text-12949124a09s70a");
-
     if (key == "<" && !(listeningForTag)) {
+        tagName = "";
         listeningForTag = true;
-    } else if (listeningForTag && isTypable(key)) {
+    } else if (isTypable(key) && listeningForTag) {
         tagName += key.toString();
     } else if (key == ">" && listeningForTag) {
         listeningForTag = false;
-
-        // from https://bennadel.com/4086
-        var code = input.value;
-        var cursorPosition = input.selectionEnd;
-        // where cursor will be after paste
-        var finalPosition = (cursorPosition + tagName.length + 3);
-        // textarea value is equal to the code up to cursor + tag + code after cursor
-        input.value = code.splice(0, cursorPos) + `</${tagName}>` + code.slice(cursorPos);
-        // move cursor
-        input.selectionStart = finalPosition;
-        input.selectionEnd = finalPosition;
-    } else if (key == "Backspace") {
+        insertString(`></${tagName}`);
+    } else if (key == "Backspace" && listeningForTag) {
         if (tagName.length > 1){
             tagName = tagName.substring(0, tagName.length - 1);
         } else {
             tagName = "";
             listeningForTag = false;
         }
+    } else if (key == "Tab") {
+        insertString("    ");
+        input.focus();
     };
+});
+
+window.addEventListener("load", (event) => {
+    input.value = `
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>My Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta charset="utf-8">
+<style>
+
+</style>
+
+</head>
+<body>
+
+<h1>My Page</h1>
+
+</body>
+</html>
+    `;
 });
