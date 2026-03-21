560725e571f68d93178b5b028d8c2a6ffe0412af
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2026-03-21 08:51:48 -0500

    maybe fixing app

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ function appInit(){
     var iframeWindow = document.getElementById('contentwindow-v9sdv907s9d0v').contentWindow;
     iframeWindow.document.open();
     // Adds code to iframe to alert us to console events. Obviously see iframe.js.
-    iframeWindow.document.write("<script src='https://pajamaclaws.net/babyhtml/iframe.js'></script>");
+    iframeWindow.document.write(`
+        var source;
+        // See https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+        window.addEventListener("message", (event) => {
+            alert(["iframe: ", event.data].join(""));
+            source = event.source;
+            source.postMessage("pong");
+
+            // handle console functions
+            console.clear = () => source.postMessage("clear");
+            console.log = (x) => source.postMessage(JSON.stringify(["log", x]));
+            console.error = (x) => source.postMessage(JSON.stringify(["error", x]));
+            console.debug = (x) => source.postMessage(JSON.stringify(["debug", x]));
+            console.info = (x) => source.postMessage(JSON.stringify(["info", x]));
+            console.warn = (x) => source.postMessage(JSON.stringify(["warn", x]));
+        });
+
+        // Handle JS errors
+        window.onerror = (message, loc, lineno, colno, error) => console.error(["error at ", loc, " (", lineno, ",", colno, "): ", message].join(""));
+    `);
     iframeWindow.document.close();
 
     // setting up custom console
